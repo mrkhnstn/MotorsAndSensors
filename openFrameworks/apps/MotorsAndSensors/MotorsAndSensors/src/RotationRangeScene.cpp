8d9475897ecc3887418bce5305adfe6110de296e
Miquel Lopez <miquel7@gmail.com>
2012-01-19 12:54:06 +0100

    Pending commit
    Small changes over yesterday's commit as it wasn't pushed to master.
    New commit with the new changes to follow soon.

--- openFrameworks/apps/MotorsAndSensors/MotorsAndSensors/src/RotationRangeScene.cpp
+++ openFrameworks/apps/MotorsAndSensors/MotorsAndSensors/src/RotationRangeScene.cpp
@@ -1,11 +1,7 @@
 /*
  *  RotationRangeScene.cpp
- *  MotorsAndSensors_Miquel
  *
  *  Created by Miquel Lopez on 17/01/12.
- *  Copyright 2012 ozke.com. All rights reserved.
  *
  */
-
 #include "RotationRangeScene.h"
-
--- openFrameworks/apps/MotorsAndSensors/MotorsAndSensors/src/RotationRangeScene.h
+++ openFrameworks/apps/MotorsAndSensors/MotorsAndSensors/src/RotationRangeScene.h
@@ -1,6 +1,8 @@
 #pragma once
 /*
  *  RotationRangeScene.h
+ *		This class creates an linear animation where panels move from a minimum to a maximum angle.
+ *		Different animation modes will define different versions of the animation
  *
  *  Created by Miquel Lopez on 17/01/12.
  *
@@ -8,29 +10,37 @@
 
 #include "Scene.h"
 
-/*
- This class creates a wave effect animation.
- */
 class RotationRangeScene : public Scene {
 	
 public:
 	int direction;						// Two possible states: 1 (increasing) or -1 (decreasing)
 	float animationIndex;				// Ranges from -1 to 1
 	float minRotation, maxRotation;		// Minimum and maximum rotation
+	float velocity;						// Animations velocity
 	float angle;
-	float velocity;
+
+	// Defines the type of animation
+	int animationMode;
+	static const int NORMAL = 0;
+	static const int ODD_EVEN = 1;
 	
+	// List of variable presets
+	bool setPreset1, setPreset2;
+		
 	void setup(){
 		Scene::setup();
 		name = "RotationRangeScene";
 		
 		animationIndex = 0;
-		minRotation = 75;
-		maxRotation = 105;
-		velocity = .01;
-		
+		minRotation = 0;
+		maxRotation = 180;
+		velocity = .03;
 		angle = 90;
 		direction = 1;
+		
+		animationMode = NORMAL;
+		setPreset1 = false;
+		setPreset2 = false;
 	}
 	
 	void setupGUI(){
@@ -38,9 +48,16 @@ public:
 		gui.addSlider("minRotation", minRotation, 0, 180);
 		gui.addSlider("maxRotation", maxRotation, 0, 180);
 		gui.addSlider("velocity", velocity, 0, .1);
-		
 		gui.addSlider("animationIndex", animationIndex, -1, 1);
 		gui.addSlider("angle", angle, 0, 180);
+		
+		string modeChoices[2] = {"NORMAL","ODD_EVEN"};
+		gui.addComboBox("animationMode", animationMode, 2, modeChoices);
+		
+		// Presets
+		gui.addTitle("Variable presets").setNewColumn(true);
+		gui.addButton("Preset 1", setPreset1);
+		gui.addButton("Preset 2", setPreset2);
 	}
 	
 	void postGUI(){
@@ -54,6 +71,12 @@ public:
 	void update(){
 		Scene::update();
 		
+		// Detect presets
+		if(setPreset1 == true)
+			setPreset(1);
+		if(setPreset2 == true)
+			setPreset(2);
+		
 		// Animation sequence, animationIndex goes from -1 to 1 and back
 		animationIndex += direction*velocity;
 		if (animationIndex > 1) {
@@ -64,9 +87,24 @@ public:
 		
 		// angle of the motors defined by rotation range (the maximum it rotates from side to side)
 		// and animationIndex (between -1 and 1)
-		angle = ofMap(animationIndex, -1, 1, minRotation, maxRotation);
-		for(int i=0; i<getMotorCount(); i++)
+		// angle = ofMap(animationIndex, -1, 1, minRotation, maxRotation);
+		for(int i=0; i<getMotorCount(); i++){
+			switch (animationMode) {
+				case NORMAL:
+					angle = ofMap(animationIndex, -1, 1, minRotation, maxRotation);
+					break;
+				case ODD_EVEN:
+					if(i%2 == 0){
+						angle = ofMap(animationIndex, -1, 1, minRotation, maxRotation);
+					}else{
+						angle = ofMap(-animationIndex, -1, 1, minRotation, maxRotation);
+					}
+					break;
+				default:
+					break;
+			}
 			setMotorAngle(i, angle);
+		}
 	}
 	
 	void draw(){
@@ -76,4 +114,21 @@ public:
 	void stop(){
 		Scene::stop();
 	}
+	
+	
+	void setPreset(int presetNum){
+		switch(presetNum){
+			case 1:
+				minRotation = 75;
+				maxRotation = 105;
+				break;
+			case 2:
+				minRotation = 0;
+				maxRotation = 180;
+				break;
+			default:
+				break;
+		}
+	}
+	
 };
